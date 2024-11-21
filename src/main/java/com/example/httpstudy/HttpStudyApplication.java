d0fc11e29dc9e66b6e5ba8963e9f8387cb5081a3
LeeChang_Mac <dlckd125@nate.com>
2024-11-21 21:51:56 +0900

    first commit

---
+++ src/main/java/com/example/httpstudy/HttpStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/httpstudy/HttpStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
